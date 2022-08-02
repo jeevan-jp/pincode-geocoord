6fbb07de29c1e65ab03e321be16fd80c1489fae4
Jeevan Prakash <jiwanprakash65@gmail.com>
2022-08-02 18:08:42 +0530

    add compiled js files

--- index.ts
+++
@@ -1,7 +0,0 @@
-import PincodeData from "./data.json";
-
-function getGeoCoordsFromPincode(pincode: number | string): { lat: number, lng: number } | null {
-  return PincodeData[pincode];
-};
-
-export default getGeoCoordsFromPincode;
