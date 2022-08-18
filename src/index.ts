6a8e6e97c43fc943924999135b69f35d061cb86c
aditya3wh <aditya.banotra@wherehouse.io>
2022-08-18 18:12:26 +0530

    Search with comma seperated city

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import PincodeData from "./PincodeToGeoCode";
 import pincodeToCity from "./pincodeToCity";
 import Fuse from "fuse.js";
 import { cities } from "./DelhiveryCities";
+import _ from "lodash";
 
 function getGeoCoordsFromPincode(
   pincode: number | string
@@ -9,11 +10,39 @@ function getGeoCoordsFromPincode(
   return PincodeData[pincode];
 }
 
-function generateCityNameToCalcZone(city: string): string {
-  const fuse = new Fuse(cities);
+function generateCityNameToCalcZone(city: string): string | undefined {
+  const options = {
+    includeScore: true,
+  };
+  const fuse = new Fuse(cities, options);
 
-  const result = fuse.search(city);
+  let result = fuse.search(city);
 
+  if (result.length === 0) {
+    if (city.indexOf(",")) {
+      const cities = city.split(",");
+
+      let score = 1;
+
+      for (let city of cities) {
+        city = _.trim(city);
+        const res = fuse.search(city);
+        if (typeof res?.[0]?.score !== "undefined" && res?.[0]?.score < score) {
+          score = res[0].score;
+          result = res;
+        }
+      }
+    }
+  }
+
+  if (typeof result[0].score !== "undefined") {
+    console.log(`Found the city with ${(1 - result[0].score) * 100}% accuracy`);
+  }
+
+  if (typeof result?.[0]?.score !== "undefined" && result?.[0]?.score > 0.4) {
+    return undefined;
+  }
   return result[0]?.item;
 }
+
 export { getGeoCoordsFromPincode, pincodeToCity, generateCityNameToCalcZone };
